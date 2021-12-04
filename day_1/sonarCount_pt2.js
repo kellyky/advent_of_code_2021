fa1821971ead118fba930bf4cb2d27bcdf655887
kellyky <kelpopko@gmail.com>
2021-12-04 16:18:01 -0600

    Day1 Part2 complete

--- day_1/sonarCount_pt2.js
+++ day_1/sonarCount_pt2.js
@@ -1,41 +1,37 @@
 // Part 2 https://adventofcode.com/2021/day/1#part2
 
 // Getting input & turning into array - input is same
-// const fs = require("fs");
+const fs = require("fs");
 
-// const input = fs
-//   .readFileSync("input.txt", { encoding: "utf-8" })
-//   .split("\n")
-//   .filter((x) => Boolean(x))
-//   .map((x) => parseInt(x));
+const input = fs
+  .readFileSync("input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => Boolean(x))
+  .map((x) => parseInt(x));
 
 
 // Day1 Part2:
 // Compare sum of 3 nums, with sum of previous 3
 
-const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-
 const sonarIncreaseCount = (input) => {
-  let counter = 0;
+  let counter = 0; 
 
+  for (let i = 0; i < input.length; i++) {
     let window = 0;
-    let sumA = 0;
-    let sumB = 0;
-    while (window < 1){
-      let sumA = input[window] + input[window + 1] + input[window + 2];
-      let sumB = input[window + 1] + input[window + 2] + input[window + 3];
 
-      // temp - just helps me track things
-      console.log(window);
-      console.log('sumA = ' + sumA);
-      console.log('sumB = ' + sumB);
-      // checks whether SumB is greater
+    while (window < 1){
+      let sumA = input[i] + input[i + 1] + input[i + 2];
+      let sumB = input[i + 1] + input[i + 2] + input[i + 3];
+
       if (sumB > sumA) {
         counter++;
         console.log('sumB wins!');
+      } else {
+        console.log('no points awarded');
       }
       window++;
     }
+  }
   console.log(counter);
 
 };
