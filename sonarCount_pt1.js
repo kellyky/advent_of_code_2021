8f44d6ee4bab656b7f1748349407c2b455eabd7b
kellyky <kelpopko@gmail.com>
2021-12-04 11:02:17 -0600

    Day 1, Part 1 complete

---
+++ sonarCount_pt1.js
@@ -0,0 +1,23 @@
+// Part 1: https://adventofcode.com/2021/day/1
+
+const fs = require("fs");
+
+const input = fs
+  .readFileSync("input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => Boolean(x))
+  .map((x) => parseInt(x));
+
+const sonarIncreaseCount = (input) => {
+  let counter = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] < input[i + 1]){
+      counter++;
+    }
+  }
+  
+  return counter;
+};
+
+console.log(sonarIncreaseCount(input));
