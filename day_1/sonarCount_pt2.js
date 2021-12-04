21f19186a42cc5b71b275493b91189c6a3a69c4b
kellyky <kelpopko@gmail.com>
2021-12-04 16:02:01 -0600

    Add to day1/part2 the ability to compare sums

--- day_1/sonarCount_pt2.js
+++ day_1/sonarCount_pt2.js
@@ -1,30 +1,43 @@
 // Part 2 https://adventofcode.com/2021/day/1#part2
 
 // Getting input & turning into array - input is same
-const fs = require("fs");
+// const fs = require("fs");
 
-const input = fs
-  .readFileSync("input.txt", { encoding: "utf-8" })
-  .split("\n")
-  .filter((x) => Boolean(x))
-  .map((x) => parseInt(x));
+// const input = fs
+//   .readFileSync("input.txt", { encoding: "utf-8" })
+//   .split("\n")
+//   .filter((x) => Boolean(x))
+//   .map((x) => parseInt(x));
 
 
 // Day1 Part2:
 // Compare sum of 3 nums, with sum of previous 3
 
-
+const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
 const sonarIncreaseCount = (input) => {
-  // vars to hold the two sums as we go, so we can compare them
-  let sumA = 0;
-  let sumB = 0;
   let counter = 0;
 
-  // code for comparing sums A and B as we go
-  // counter++
-  
-  return counter;
+    let window = 0;
+    let sumA = 0;
+    let sumB = 0;
+    while (window < 1){
+      let sumA = input[window] + input[window + 1] + input[window + 2];
+      let sumB = input[window + 1] + input[window + 2] + input[window + 3];
+
+      // temp - just helps me track things
+      console.log(window);
+      console.log('sumA = ' + sumA);
+      console.log('sumB = ' + sumB);
+      // checks whether SumB is greater
+      if (sumB > sumA) {
+        counter++;
+        console.log('sumB wins!');
+      }
+      window++;
+    }
+  console.log(counter);
+
 };
 
 console.log(sonarIncreaseCount(input));
