eb0a8c202b28aa43f2a11e5fe5e6d5e487087da5
kellyky <kelpopko@gmail.com>
2021-12-06 19:10:05 -0600

    File cleanup & organization

--- day04/day04-part2.js
+++
@@ -1,9 +0,0 @@
-// Code I can use to 'import' the daily input file
-
-const fs = require("fs");
-
-const input = fs
-  .readFileSync("input.txt", { encoding: "utf-8" })
-  .split("\n")
-  .filter((x) => Boolean(x))
-  .map((x) => parseInt(x));
