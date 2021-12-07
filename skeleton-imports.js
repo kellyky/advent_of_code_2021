b30245108770ac2276d689456e3a99f30e8c2546
kellyky <kelpopko@gmail.com>
2021-12-06 19:21:00 -0600

    Swap boolean for simpler version

--- skeleton-imports.js
+++ skeleton-imports.js
@@ -5,5 +5,5 @@ const fs = require("fs");
 const input = fs
   .readFileSync("input.txt", { encoding: "utf-8" })
   .split("\n")
-  .filter((x) => Boolean(x))
+  .filter((x) => x)
   .map((x) => parseInt(x));
