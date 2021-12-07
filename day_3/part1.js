3647f168f13c42bbe6b5f3c84102db7de4c589e9
kellyky <kelpopko@gmail.com>
2021-12-07 17:15:18 -0600

    Add check length of input, can apparently grab vertical slice of array to new array

--- day_3/part1.js
+++ day_3/part1.js
@@ -10,16 +10,27 @@
 // Day 3: https://adventofcode.com/2021/day/3
 //
 
-const input = ['10', '01'];
 
-// 2-D array-izer TEMPLATE
-// for (let i = 0; i < input.length; i++) {
-//   const entry = input[i];
-//   for (let j = 0; j < entry.length; j++) {
-//     const cell = Array.from(entry);
-//     console.log(cell);
+const sum = (x, y) => x + y;
+const length = (numbers) => numbers.length;
+const numbers = ['00100', '11110', '10110'];
 
-//   }
-// }
+let indicesI = (numbers, index) => {
+  let numArrI = [];
+  for (let i in numbers) {
+    const entry = numbers[i];
+    numArrI.push(entry[index]);
+  }
+  return numArrI;
+}
 
-
+const gammaAdder = indecesI => {
+  let gamma = 0;
+  indicesI.
+  sum < length / 2 ? gamma += 1 : gamma += 0;
+  return gamma;
+}
+// console.log(indicesI).reduce(add);
+console.log('length: ' + length(numbers));
+console.log('indices returns: ' + indicesI(numbers, 0));
+console.log(gammaAdder(indicesI));
