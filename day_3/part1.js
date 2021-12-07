41b8356b80bc1f0502680d4c3660a085e112c4b8
kellyky <kelpopko@gmail.com>
2021-12-06 23:51:24 -0600

    2-D array mini template

--- day_3/part1.js
+++ day_3/part1.js
@@ -1,9 +1,25 @@
-// Code I can use to 'import' the daily input file
+// reminder to update the input file to input.txt
+// const fs = require("fs");
 
-const fs = require("fs");
+// const input = fs
+//   .readFileSync("sampleInput.txt", { encoding: "utf-8" })
+//   .split("\n")
+//   .filter((x) => x)
+  // .map((x) => parseInt(x));
 
-const input = fs
-  .readFileSync("input.txt", { encoding: "utf-8" })
-  .split("\n")
-  .filter((x) => Boolean(x))
-  .map((x) => parseInt(x));
+// Day 3: https://adventofcode.com/2021/day/3
+//
+
+const input = ['10', '01'];
+
+// 2-D array-izer TEMPLATE
+// for (let i = 0; i < input.length; i++) {
+//   const entry = input[i];
+//   for (let j = 0; j < entry.length; j++) {
+//     const cell = Array.from(entry);
+//     console.log(cell);
+
+//   }
+// }
+
+
