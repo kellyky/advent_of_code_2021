635f25a5f3bebddc872087a874e54de29b78f4df
kellyky <kelpopko@gmail.com>
2021-12-07 21:48:45 -0600

    Adds const for sum, length, and ability to grab a veritical slice o' array

--- day_3/part1.js
+++ day_3/part1.js
@@ -8,29 +8,42 @@
   // .map((x) => parseInt(x));
 
 // Day 3: https://adventofcode.com/2021/day/3
-//
 
 
+// sum of vertical index-slice of array (I suspect this boilerplate formulat needs some work)
 const sum = (x, y) => x + y;
+
+// length calculates length of input - numbers or whatever I rename it to
 const length = (numbers) => numbers.length;
+
+// placeholder set, will need to change this out for the real input
 const numbers = ['00100', '11110', '10110'];
 
-let indicesI = (numbers, index) => {
-  let numArrI = [];
+const numArrI = (numbers, index) => {
+  let arr = [];
   for (let i in numbers) {
     const entry = numbers[i];
-    numArrI.push(entry[index]);
+    arr.push(entry[index]);
   }
-  return numArrI;
+  return arr;
 }
 
-const gammaAdder = indecesI => {
+
+// function returns array of numbers[index]
+// let indicesI = (numbers, index) => {
+//   let numArrI = [];
+//   let gamma = 0;
+//   let epsilon = 0;
+
+// }
+
+// Once it's working, it'll calculate the gamma for the indicesI :-)
+const gammaAdder = indicesI => {
   let gamma = 0;
-  indicesI.
   sum < length / 2 ? gamma += 1 : gamma += 0;
   return gamma;
 }
+
 // console.log(indicesI).reduce(add);
 console.log('length: ' + length(numbers));
-console.log('indices returns: ' + indicesI(numbers, 0));
-console.log(gammaAdder(indicesI));
+console.log('numArrI: ' + numArrI(numbers, 2));
