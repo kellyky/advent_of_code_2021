f62bd20aca992ca2af179956d1f06ecfba978430
kellyky <kelpopko@gmail.com>
2021-12-09 00:22:22 -0600

    Adds accurate count of ones/zeroes in column/array slice. Also orchestral names for a fresh look

--- day_3/part1.js
+++ day_3/part1.js
@@ -9,11 +9,9 @@ const readings = fs
 
 
 const sum = (x, y) => x + y;
-const length = (readings) => readings.length;
 const arraySum = array => array.reduce(sum);
-const indices = [0, 1, 2, 3, 4];
 
-const columnGetter = (readings, ...index) => {
+const powerSlice = (readings, index) => {
   let column = [];
   for (let i in readings) {
     const entry = readings[i];
@@ -22,13 +20,75 @@ const columnGetter = (readings, ...index) => {
   return column;
 }
 
-const column0 = columnGetter(readings, 0);
-const column1 = columnGetter(readings, 1);
-const column2 = columnGetter(readings, 2);
-const column3 = columnGetter(readings, 3);
-const column4 = columnGetter(readings, 4);
 
 
-const allColumns = [columnGetter(readings, 0), columnGetter(readings, 1), columnGetter(readings, 2), columnGetter(readings, 3), columnGetter(readings, 4)]
+const allColumns = [powerSlice(readings, 0), powerSlice(readings, 1), powerSlice(readings, 2), powerSlice(readings, 3), powerSlice(readings, 4)]
+console.log(allColumns);
 
-allColumns.forEach(col => console.log(col));
+const orchestra = allColumns
+  .map((violin => { 
+    const soloists = violin.filter(x => x == 1);
+    const audience = violin.filter(y => y == 0);
+    console.log(soloists.length+ ' ' + audience.length);
+
+
+
+    let onesCount = violin.reduce(function (count, cello) {
+      cello in count ? count[cello]++ : count[cello] = 1;
+      console.log(count);
+      return count
+    }, {})
+
+  }))
+
+
+
+
+
+
+  // let gamma = placeholder => {
+  // const onesInPowerSlice = violin.filter(x => x == 1);
+  // onesInPowerSlice.length > readings.length
+  //   ? gamma += 1
+  //   : gamma += 0;
+    
+  //   return gamma;
+
+  // }
+
+
+  // let epsilon = [];
+  // const zeroesInPowerSlice = violin.filter(x => x == 0);
+  // zeroesInPowerSlice.length > readings.length
+  //   ? epsilon.push(0)
+  //   : epsilon.push(1);
+
+  // console.log(violin)}
+  // ))
+
+    
+
+// console.log(allColumns);
+
+
+// const readOnes = allColumns.forEach(col => {
+//   col.filter(x => x == 1);
+// })
+// const powerReader = allColumns.forEach(col => {
+//   const onesInSlice = col.filter(x => x == 1);
+// })
+
+// const gamma = (onesInPowerSlice => {
+//       onesInPowerSlice.length > rereadings.length
+//       ? gamma.push(1)
+//       : gamma.push(0);
+//       return gamma;
+//     })
+
+//     const epsilon = (onesInPowerSlice => {
+//       onesInPowerSlice.length > zeroesInPowerSlice
+//       ? epsilon.push(0)
+//       : epsilon.push(1);
+//       return epsilon;
+//     })
+
