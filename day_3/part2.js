06262c42d521f336840080d0582453e3214b8242
kellyky <kelpopko@gmail.com>
2021-12-18 22:49:57 -0600

    Successfully naming whether bit has more ones or zeros

--- day_3/part2.js
+++ day_3/part2.js
@@ -8,7 +8,7 @@ const readings = fs
 // Day 3: https://adventofcode.com/2021/day/3
 
 const sum = (x, y) => x + y;
-const listLength = readings.length;
+const totalReadingsCount = readings.length;
 const wordLength = readings[0].length;
 const arraySum = array => array.reduce(sum);
 
@@ -24,8 +24,8 @@ const allTheBitSlices = array => {
   let newestArr = [];
 
   // (x, y) are slice coords
-  for (let x = 0; x < (listLength * wordLength); x += listLength){
-    let y = x + listLength;
+  for (let x = 0; x < (totalReadingsCount * wordLength); x += totalReadingsCount){
+    let y = x + totalReadingsCount;
     newestArr.push(flattened.slice(x, y));
   }
   return newestArr;
@@ -39,7 +39,7 @@ const massiveArrOfOnes = bitIndexReadings.map((column => {
 
 
 const gamma = massiveArrOfOnes.map( countedOnes => {
-  return countedOnes.length > listLength/2 ? '1' : '0';
+  return countedOnes.length > totalReadingsCount/2 ? '1' : '0';
 })
 
 const gammaToDecimal = gamma => {
@@ -61,55 +61,80 @@ const powerReading = (gammaToDecimal, epsilonToDecimal) => {
   return power;
 }
 
-console.log(powerReading(gammaToDecimal, epsilonToDecimal));
+// console.log(powerReading(gammaToDecimal, epsilonToDecimal));
 
 // life support rating = oxygen generator rating * CO2 scrubber rating
 // oxigen rating => most common
 // CO2 scrubber => least common
 
-const bergamot = readings.map(reading => {
+const bitOneCount = arr => {
   return bitIndexReadings.map(el => {
     const ones = el.filter(n => n == 1);
     return ones.length;
   })
-})
-
-
-// goes through each element of bitIndexReadings
-const lavender = bitIndexReadings.map( el => {
-  
-  
-
-  // if element of bitI.. has more 1s (or equal):
-  //    filter/keep those elements from readings
-  //if element of bitI.. has more 0sm 
-  //    filter/keep those elements from readings
-})
-
-
-
-                          
-console.log("bergamot creates array of ones in bit; returns length of that array");
-console.log(bergamot);
-
-
-
-
-
-
-
-
-
-
-console.log(listLength/2);
-
-const whichBits = bergamot => {
-  if (bergamot >= listLength / 2) return "keep bit with 1s"
-  else return "keep bits with 0s";
 }
 
-const geranium = readings.filter(whichBits);
+const sweetOrange = bitOneCount(readings);
+
+console.log(sweetOrange);
+
+// const whichValGreater = arr => {
+//   let eval = [];
+//   for (let el of arr) {
+//     if (el >= totalReadingsCount / 2) {
+//       eval.push("ones");
+//     } else {
+//       eval.push("zeros");
+//     }
+//   }
+//   return eval
+// }
 
 
 
+// evaluates whether 
+const beaker = (arr1, arr2) => {
+  let eval = [];
+  for (let el of arr1){
+    if (el >= totalReadingsCount / 2) {
+      eval.push("yes");
+    } else {
+      eval.push("not this time");
+    }
+  }
+  return eval;
+}
+
+console.log(beaker(sweetOrange, readings));
+
+
+  
+  
+
+
+
+
+
+
+// const tangerine = sweetOrange.map(el => {
+
+
+//   if (sweetOrange[el] >= (totalReadingsCount / 2)){
+//     console.log("more ones");
+//   } else {
+//     console.log("more zeros");
+//   }
+// })
+
+
+
+
+
+///u goes through each element of bitIndexReadings
+//const lavender = bitIndexReadings.map( el => {
+//  // if element of bitI.. has more 1s (or equal):
+//  //    filter/keep those elements from readings
+//  //if element of bitI.. has more 0sm 
+//  //    filter/keep those elements from readings
+//})
 
