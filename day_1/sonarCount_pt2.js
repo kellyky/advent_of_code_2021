6cdb49bc14cd01a698aabe5446c071f19e347cd0
kellyky <kelpopko@gmail.com>
2022-01-07 10:53:19 -0600

    Refactor, simplify a bit

--- day_1/sonarCount_pt2.js
+++ day_1/sonarCount_pt2.js
@@ -1,24 +1,23 @@
 // Part 2 https://adventofcode.com/2021/day/1#part2
-
-// Getting input & turning into array - input is same
 const fs = require("fs");
 
 const input = fs
   .readFileSync("input.txt", { encoding: "utf-8" })
   .split("\n")
-  .filter((x) => Boolean(x))
+  .filter(x => x)
   .map((x) => parseInt(x));
 
 
 // Day1 Part2:
-// Compare sum of 3 nums, with sum of previous 3
-
 function getSonarIncreaseCount (arr) {
   let counter = 0;
-  for (let i in arr) {
-    let sumA = arr[i] + arr[i + 1] + arr[i + 2];
-    let sumB = arr[i + 1] + arr[i + 2] + arr[i + 3];
-    if (sumB > sumA) counter++;
+
+  for (let i = 0; i < arr.length; i++){
+    let firstSum = arr[i] + arr[i + 1] + arr[i + 2];
+    let secondSum = arr[i + 1] + arr[i + 2] + arr[i + 3];
+
+    if (secondSum > firstSum) counter++;
+
   }
   return counter;
 }
