4ab34a8d22bf1fc137d9d9855ffe1b2550432232
kellyky <kelpopko@gmail.com>
2022-01-31 00:16:55 -0600

    Cleanup the mess

--- day_7/part1.js
+++ day_7/part1.js
@@ -7,61 +7,21 @@ const input = fs
   .filter((x) => x)
   .map((x) => parseInt(x));
 
-// Max and Min probably won't be used irl but we'll see. I'm using them to hone in on how to figure out valid horizontal horizontalPositions to consider
-let inputMax = Math.max(...input);
-console.log("Biggest number from input");
-console.log(inputMax);
-console.log("\n");
-
-
-let inputMin = Math.min(...input);
-console.log("Smallest number from input");
-console.log(inputMin);
-console.log("\n");
-
-
-// Takes the average of values in input
-// I don't think this wll be used but we'll see
-let mean = arr => (arr.reduce((x,y) => x + y)) / arr.length;
-console.log(mean(input));
-
-// Median of input. Thinking it'll be median or mode we'll need... 
-let median = arr => {
+const median = arr => {
+  const length = arr.length;
   arr.sort((a, b) => a - b);
 
-  if (arr.length % 2 === 0) {
-    return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
+  if (length % 2 === 0) {
+    return (arr[length / 2 - 1] + arr[length / 2]) / 2;
   }
-  return arr[(arr.length - 1) / 2];
+  return arr[(length - 1) / 2];
 }
 
-console.log(median(input));
+const horizontalPosition = median(input);
 
+// FUEL used for each move in array
+const fuel = input.map(x => Math.abs(x - horizontalPosition));
 
-// let horizontalPosition = (inputMax + inputMin) / 2
-let horizontalPosition = median(input);
-let tryingTheseStartingPoints = input;
-
-console.log("Horizontal Position: This is the value we're using to determine fuel cost");
-console.log(horizontalPosition);
-console.log("\n");
-
-// FUEL used; creates array from input and horizontal horizontalPosition
-let fuel = input.map(x => Math.abs(x - horizontalPosition));
-console.log(`Individual fuel cost: ${fuel}\n`);
-
-// Things I still don't know
-// What are the valid horizontalPositions? How do I know when to stop trying new numbers? Possibly within a certain range of the highest & lowest? 
-
-
-
-
-
-
-
-
-
-
-// Total fuel spent; should be the final step for part 1
-let totalFuelSpent = fuel.reduce(( x, y ) => x + y);
+// Total fuel spent
+const totalFuelSpent = fuel.reduce(( x, y ) => x + y);
 console.log(totalFuelSpent);
