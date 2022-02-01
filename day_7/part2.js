10fdbb4c629e8de5ebd583f094f9e41cd73a0a58
kellyky <kelpopko@gmail.com>
2022-02-01 15:15:59 -0600

    Add function that takes e.g. 4 steps and returns e.g. 10 fuel (4 + 3 + 2 + 1)

--- day_7/part2.js
+++ day_7/part2.js
@@ -1,12 +1,13 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const input = fs
-  .readFileSync("input.txt", { encoding: "utf-8" })
-  .split(",")
-  .filter((x) => x)
-  .map((x) => parseInt(x));
+// const input = fs
+//   .readFileSync("input.txt", { encoding: "utf-8" })
+//   .split(",")
+//   .filter((x) => x)
+//   .map((x) => parseInt(x));
 
-// const input = [16,1,2,0,4,2,7,1,2,14];
+// Sample input
+const input = [16,1,2,0,4,2,7,1,2,14];
 
 const median = arr => {
   const length = arr.length;
@@ -18,11 +19,30 @@ const median = arr => {
   return arr[(length - 1) / 2];
 }
 
-// Part 1 used median
+// PART ONE -- Used Median
 const horizontalPosition = median(input);
-
-// console.log(`Median: ${horizontalPosition}`);
-
-// PART 1 -- FUEL used for each move in array
 const fuelPart1 = input.map(x => Math.abs(x - horizontalPosition));
-// For part 1, we're just using the median for the best distance. Something about the middle of the spectrum being a good starting point aor shot in the dark. 
+
+/* --------------- PART 2 ----------------------------------------------------------------------------
+ *
+ * I have TWO problems to solve here, I think:
+ * (1) Optimal distance
+ * (2) Get the sum for fuel 
+ *      NEW pattern for fuel:
+ *      - 1 step: 1 fuel
+ *      - 2 step: 2 fuel + 1 step
+ *      - 3 step: 3 fuel + 2 step
+ *      - n step: n fuel + (n-1) step 
+
+/* ------------- Let's try Problem 2 first ----------------- */
+
+// Just gest fuel count alone based on steps input - e.g. 2 steps returns 3 fuel (i.e. 2 + 1 fuel)
+function fuelz(step) {
+  let fuel = 0;
+  while (step > 0) {
+    fuel += step;
+    step--;
+  }
+  return fuel;
+}
+
