dcc3ae5c7b80d0144e805fee8e928d534fd49504
kellyky <kelpopko@gmail.com>
2022-02-01 21:31:49 -0600

    Add day 6 skeleton files only

---
+++ day_6/part1.js
@@ -0,0 +1,13 @@
+// Code I can use to 'import' the daily input file
+
+// const fs = require("fs");
+
+// const input = fs
+//   .readFileSync("input.txt", { encoding: "utf-8" })
+//   .split("\n")
+//   .filter((x) => x)
+//   .map((x) => parseInt(x));
+
+// Sample input 
+const input = [3,4,3,1,2];
+
