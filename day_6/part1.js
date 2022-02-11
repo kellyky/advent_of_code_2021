897d127959dda5cd2d25e54a994191416445c789
kellyky <kelpopko@gmail.com>
2022-02-10 18:20:03 -0600

    Decrements array values  for each day

--- day_6/part1.js
+++ day_6/part1.js
@@ -1,8 +1,11 @@
 // Code I can use to 'import' the daily input file
+//
+// Sample input 
+let starterPopulation = [3,4,3,1,2];
 
 // const fs = require("fs");
 
-// const internalTimerOfEachFish = fs
+// const starterPopulation = fs
 //   .readFileSync("input.txt", { encoding: "utf-8" })
 //   .split(",")
 //   .filter((x) => x)
@@ -10,30 +13,34 @@
 
 
 /* --------- THINGS WE KNOW ------------------
- * Each entry is the internal timer of each fish
- *    - Each day, the timer ticks down by 1
- *    - When the timer reaches 0, 2 things happen:
- *        - (1) The PARENT fish timer resets to 6
- *        - (2) The CHILD fish has a timer of 8
- *            - (a) The CHILD fish does not start counting down until the following day
- *
-
- -------------- HOW can I BREAK THIS DOWN --------------
- Well, which problems do I need to solve?
- (1) Each "day" the internalTimerOfEachFish ticks down 
+ (1) Each "day" the internalTimerOfEachFish ticks down
  (2) When any individual fish timers hit `0`:
     (a) a new fish timer is born (i.e. arr.push(8))
     (b) the fish with timer `0` gets reset to 6
-
+ (3) We get the fish population with arr.length once the days have all run
 */
 
-// Sample input 
-const internalTimerOfEachFish = [3,4,3,1,2];
+// let days = 18;
 
-const timerTicker = arr => {
-  return arr.map( x => x - 1);
+const population = arr => {
+  let day = 0;
+
+  while (day <= 18 ){
+    console.log(day);
+
+    arr = arr.map(x => {
+      if (x === 0) {
+        console.log("We need new fish & a timer restart!");
+      } 
+      return x - 1;
+    });
+
+    console.log(arr);
+    day++;
+  }
+  return arr;
 }
 
-console.log (timerTicker(internalTimerOfEachFish));
+console.log(population(starterPopulation));
 
 
