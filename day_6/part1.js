bb1b46385f069bc4aef44c90a30f6193d7af8f3a
kellyky <kelpopko@gmail.com>
2022-02-11 14:42:22 -0600

    New fish now adds 8; count stll wrong

--- day_6/part1.js
+++ day_6/part1.js
@@ -10,22 +10,50 @@
 // Sample input 
 let starterPopulation = [3,4,3,1,2]; // once test info works, comment this out and uncomment the above
 
+// const population = arr => {
+//   for (let day = 0; day < 18; day++){  // change days to 80 later
+//     console.log(`New Day: ${day}`);
+//     arr = arr.map(x => {
+//       if (x === 0) {
+//         const createThisManyFish = arr.filter(x => x === 0).length;
+//         const tempNewFish = Array(createThisManyFish).fill(8);
+//         console.log(tempNewFish);
+//         return x += 6;
+//       } 
+//       return x - 1;
+//     });
+//     console.log(arr); // temp; to see if the values are updating correctly
+//   }
+//   return arr.length;
+// }
+
+// let starterPopulation = [3,4,3,1,2];
+
 const population = arr => {
-  for (let day = 0; day < 18; day++){  // change days to 80 later
-    console.log(`New Day: ${day}`);
+  for (let day = 1; day < 18; day++){
+
+    console.log(`New Day: ${day}`); // temp just to track days
     arr = arr.map(x => {
       if (x === 0) {
-        const createThisManyFish = arr.filter(x => x === 0).length;
-        const tempNewFish = Array(createThisManyFish).fill(8);
-        console.log(tempNewFish);
         return x += 6;
-      } 
+      }
       return x - 1;
     });
-    console.log(arr); // temp; to see if the values are updating correctly
+
+    const createThisManyFish = arr.filter(x => x === 0).length;
+    const tempNewFish = Array(createThisManyFish).fill(8);
+    // console.log(tempNewFish);
+
+    arr = arr.concat(tempNewFish);
+    console.log(arr)
+
+
+//    console.log(arr.concat(tempNewFish)); // temp; to see if the values are updating correctly
   }
   return arr.length;
 }
+
+console.log(population(starterPopulation));
 
 console.log(population(starterPopulation));
 
