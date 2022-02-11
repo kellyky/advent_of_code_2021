2fa929eed7dc934d92323c53687f54e7141a5f7d
kellyky <kelpopko@gmail.com>
2022-02-10 18:59:08 -0600

    Create new Array of 'Hatchlings' - baby fish

--- day_6/part1.js
+++ day_6/part1.js
@@ -13,14 +13,16 @@ let starterPopulation = [3,4,3,1,2];
 
 
 /* --------- THINGS WE KNOW ------------------
- (1) Each "day" the internalTimerOfEachFish ticks down
+ (x) Each "day" the internalTimerOfEachFish ticks down
  (2) When any individual fish timers hit `0`:
     (a) a new fish timer is born (i.e. arr.push(8))
-    (b) the fish with timer `0` gets reset to 6
- (3) We get the fish population with arr.length once the days have all run
+    (x) the fish with timer `0` gets reset to 6
+ (x) We get the fish population with arr.length once the days have all run
 */
 
 // let days = 18;
+
+const hatchlings = arr => arr.filter( x => x === 0);
 
 const population = arr => {
   let day = 0;
@@ -30,11 +32,16 @@ const population = arr => {
 
     arr = arr.map(x => {
       if (x === 0) {
-        console.log("We need new fish & a timer restart!");
+        const hatchlingCount = hatchlings(arr).length
+        const newHatchlings = Array(hatchlingCount).fill(8);
+        console.log(newHatchlings);
         return x += 6;
       } 
       return x - 1;
     });
+
+    // Need to find out how many '0' value elements & push or concat that many elements valued at 8 onto the array
+
 
     console.log(arr);
     day++;
