c827b03277e7a4b144ae101064991a68f5a2fc27
kellyky <kelpopko@gmail.com>
2022-02-11 18:02:13 -0600

    Cleanup

--- day_6/part1.js
+++ day_6/part1.js
@@ -10,12 +10,9 @@ let starterPopulation = [3,4,3,1,2];
 
 const population = arr => {
 
-  // looping the days
   for (let day = 0; day < 80; day++){  
     const createThisManyFish = arr.filter(x => x === 0).length;
-    // console.log(`New Day starts: ${day}`); // temp just to track days
 
-    // Ticks the counter down each day (or re-sets it to 6 if it's at 0)
     arr = arr.map(x => {      
       if (x === 0) {
         return x += 6;
@@ -24,10 +21,7 @@ const population = arr => {
     });
     
     const tempNewFish = Array(createThisManyFish).fill(8);
-  
     arr = arr.concat(tempNewFish);
-    
-    // console.log(arr)
 
   }
   return arr.length;
