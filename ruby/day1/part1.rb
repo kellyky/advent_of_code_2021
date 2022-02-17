9e12a8a062e5260311fa4f0f8403e1d0297f74f0
Kelly Young <kelpopko@gmail.com>
2022-02-17 09:23:57 -0600

    Add method; code now lives in a method

--- ruby/day1/part1.rb
+++ ruby/day1/part1.rb
@@ -1,30 +1,19 @@
 entries = File.read("input.txt").split
 
-sum = 0
-entries.each_with_index do |current, i|
-  previous = entries[i - 1]
-  next if i == 0;  
-  if current > previous
-   sum += 1
-  end
-end
-
-puts sum  # almost there? it computes the sample correctly. With real data I'm off by 1...
-
 # input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
+def get_sum(input)
+ sum = 0
+ input.each_with_index do |current, i|
+   previous = input[i - 1]
+   next if i == 0;
+   if current > previous
+     sum += 1
+   end
+ end
+ sum
+end
 
-# I don't have it working yet as a method
-# def get_sum(input)
-#   sum = 0
-#   input.each_with_index do |current, i|
-#     previous = input[i - 1]
-#     next if i == 0;
-#     if current > previous
-#       sum += 1
-#     end
-#   end
-#   sum
-# end
+the_sum = get_sum(entries)
+puts the_sum
 
-# get_sum(entries)
