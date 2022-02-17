c6ebd14c09fe3e0a2fbe52af0e0bead4be50e20a
Kelly Young <kelpopko@gmail.com>
2022-02-16 22:31:30 -0600

    Attempt lands closer. Calculations off by 1 (real data only). Also includes attempt at turning it into a method (does not work yet)

--- ruby/day1/part1.rb
+++ ruby/day1/part1.rb
@@ -1,11 +1,30 @@
-arr = File.read("input.txt").split
+entries = File.read("input.txt").split
 
-# puts file_data
 sum = 0
-arr.each_with_index do |num, i|
-  if arr[i] >= arr[i - 1]
-    sum += 1
+entries.each_with_index do |current, i|
+  previous = entries[i - 1]
+  next if i == 0;  
+  if current > previous
+   sum += 1
   end
 end
 
-puts sum
+puts sum  # almost there? it computes the sample correctly. With real data I'm off by 1...
+
+# input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+
+# I don't have it working yet as a method
+# def get_sum(input)
+#   sum = 0
+#   input.each_with_index do |current, i|
+#     previous = input[i - 1]
+#     next if i == 0;
+#     if current > previous
+#       sum += 1
+#     end
+#   end
+#   sum
+# end
+
+# get_sum(entries)
